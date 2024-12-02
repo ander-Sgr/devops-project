1a71b0d1671b1cb904b8394e7b53472a40dd7702
ander <andersonbarreto2001@gmail.com>
2024-12-02 01:12:02 +0100

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-
+SQL_HOST=db_postgres
+SQL_PORT=5432
 # will check if the database is up after apply the config from manage.sh
-if [ "$DATABASE" = "postgres" ]; then
-    echo "Waiting for PostgreSQL to start..."
-    while ! nc -z "$SQL_HOST" "$SQL_PORT"; do
-        sleep 0.1
-    done
-    echo "PostgreSQL started"
-fi
 
-# execute the manage for up the DB
+echo "Waiting for PostgreSQL to start..."
+while ! nc -zv "$SQL_HOST" "$SQL_PORT"; do
+    sleep 0.1
+done
+echo "PostgreSQL started"
+
+
+# execute the manage for up the DB DEVELOPMENT
 sh ./manage.sh
 
 # give the control to the terminal
