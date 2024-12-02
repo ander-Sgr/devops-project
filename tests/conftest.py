a3f574cfc7e5908517a6b3bc26fbf7cdeae8142b
ander <andersonbarreto2001@gmail.com>
2024-12-02 02:27:49 +0100

    fix context app

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,24 +1,23 @@
 import pytest
 from app import create_app, db
-from app.models import db
 
+# fixture for create and configure the client testing
 @pytest.fixture(scope='module')
 def test_client():
     app = create_app('testing')
-    with app.test_client as client:
-        with app.app_context():
-            db.create_all()
-        yield client
-        with app.app_context():
-            db.drop_all()
+    # give the context
+    with app.app_context():
+        db.create_all()  # Create the database for the tests
+    with app.test_client() as client:
+        yield client  # Allows the cliente use the env test
+    with app.app_context():
+        db.drop_all()  # Delelte the tables after the tests
 
+# initialice and clean the db for each test
 @pytest.fixture(scope="function")
 def init_database():
-    """
-    This fixture initialice and clean the db for eeach test
-    """
     with create_app("testing").app_context():
-        db.create_all()
-        yield
+        db.create_all()  # Create the table for each test
+        yield db  # allows the db for the tests
         db.session.remove()
-        db.drop_all()
+        db.drop_all()  # clean the db after each test
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,10 +1,7 @@
-def test_create_data(test_client):
+from app import db
+
+def test_create_data(test_client, init_database):
     from app.models import Data
     data = Data(name="Test field name")
     db.session.add(data)
     db.session.commit()
-    
-    result = Data.query.first()
-    assert result is not None
-    assert result.name == "Test field name"
-    
