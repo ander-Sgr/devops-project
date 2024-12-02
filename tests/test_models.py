257ea45e39dac8fdc519947cf87effe8f027468d
ander <andersonbarreto2001@gmail.com>
2024-12-03 00:38:21 +0100

    fix test adding an assert

--- tests/test_models.py
+++ tests/test_models.py
@@ -5,3 +5,8 @@ def test_create_data(test_client, init_database):
     data = Data(name="Test field name")
     db.session.add(data)
     db.session.commit()
+    
+    assert data.id is not None
+    retrieved_data = db.session.get(Data, data.id)
+    assert retrieved_data is not None
+    assert retrieved_data.name == "Test field name"
